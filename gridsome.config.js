2ac29852b50d785aad957b06dc42d4d0012bafb0
Samuel Horn af Rantzien <samuel@samuelhorn.com>
2020-06-18 09:50:58 +0200

    fix src

--- gridsome.config.js
+++ gridsome.config.js
@@ -60,7 +60,7 @@ module.exports = {
         statusBarStyle: 'default',
         themeColor: '#1C2024',
         backgroundColor: '#ffffff',
-        icon: 'favicon.png',
+        icon: './static/favicon.png',
         shortName: 'samuelhorn',               // Optional
         description: 'Web designer & front-end web developer from Sweden who craft smooth experiences with modern web technology, always with characteristic design and performance in focus.',  // Optional
         categories: ['development'],           // Optional
@@ -68,9 +68,9 @@ module.exports = {
         dir: 'auto',                           // Optional
         maskableIcon: true,                    // Optional
         gcmSenderId: undefined,                // Optional
-        svgFavicon: 'favicon.svg',    // Optional. Requires favicon.ico fallback
+        svgFavicon: './static/favicon.svg',    // Optional. Requires favicon.ico fallback
         msTileColor: '#1C2024',                // Optional
-        appleMaskIcon: 'favicon.png', // Optional
+        appleMaskIcon: './static/favicon.png', // Optional
         appleMaskIconColor: '#1C2024',         // Optional
       }
     }
